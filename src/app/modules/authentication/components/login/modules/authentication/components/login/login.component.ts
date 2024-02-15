e73ab9cdb959bfdb6393f97b78f294932425947a
jeffors212 <42345368+jeffors212@users.noreply.github.com>
2024-02-15 19:03:33 -0300

    loging

---
+++ src/app/modules/authentication/components/login/modules/authentication/components/login/login.component.ts
@@ -0,0 +1,35 @@
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AuthService } from '../../services/auth.service';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss']
+})
+export class LoginComponent {
+  loginForm: FormGroup;
+
+  constructor(private fb: FormBuilder, private authService: AuthService) {
+    this.loginForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required]]
+    });
+  }
+
+  onSubmit(): void {
+    if (this.loginForm.valid) {
+      const { email, password } = this.loginForm.value;
+      this.authService.login(email, password).subscribe({
+        next: (result) => {
+          // Handle successful login
+          console.log(result);
+        },
+        error: (error) => {
+          // Handle login error
+          console.error(error);
+        }
+      });
+    }
+  }
+}
